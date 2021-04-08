547be349589613be1f6c9298df2e1ba5a9414350
kneutron <50146127+kneutron@users.noreply.github.com>
2021-04-08 13:12:24 -0500

    no backticks

--- OSX/zfs-osx-diskinfo.sh
+++ OSX/zfs-osx-diskinfo.sh
@@ -3,9 +3,9 @@
 # REF: https://openzfsonosx.org/forum/viewtopic.php?f=26&t=3115
 
 # NOTE Pass media- arg from 'zpool list -v' 
-diskutil info  `ls -l  /var/run/disk/by-id | grep "$1" | awk -F" " '{print $11}' \
+diskutil info  $(ls -l  /var/run/disk/by-id | grep "$1" | awk -F" " '{print $11}' \
  | sed 's/s1//' \
- | sed 's|\/dev\/||'` \
+ | sed 's|\/dev\/||') \
  | egrep '(Device Identifier:)|(Whole:)|(Device / Media Name)|(Disk Size:)|(Device Block Size:)'
 
 exit;
