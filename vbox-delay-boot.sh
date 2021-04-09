c7bfed63cf3662aec47394f87e2907b35ccf6b08
kneutron <50146127+kneutron@users.noreply.github.com>
2021-04-09 06:19:36 -0500

    wrong dir

--- vbox-delay-boot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# REF: https://blog.jasonantman.com/2012/04/adjusting-the-virtualbox-f12-bios-boot-prompt-timeout/
-
-VBoxManage list vms
-
-vmname="$1"
-VBoxManage modifyvm "$vmname" --bioslogodisplaytime 10000
