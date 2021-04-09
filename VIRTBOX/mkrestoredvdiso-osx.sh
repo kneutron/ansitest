8e474ec221233222043539a5737bfebc1c499013
kneutron <50146127+kneutron@users.noreply.github.com>
2021-04-09 13:32:35 -0500

    cleanup + clarifications

--- VIRTBOX/mkrestoredvdiso-osx.sh
+++ VIRTBOX/mkrestoredvdiso-osx.sh
@@ -2,26 +2,26 @@
 
 # mod for osx 2019.0818
 # make a restore DVD from .fsarchive + restore script(s)
+# NOTE this does not need to be run as root
 # NOTE mkisofs is BROKEN on osx (macports and brew)
 # REF: https://www.makeuseof.com/tag/how-to-create-windows-compatible-iso-disc-images-in-mac-os-x/
 # REF: http://osxdaily.com/2012/03/16/create-iso-images-from-the-command-line/
 
-d2b=$1
-##'/mnt/cdtemp'
-
-cd $d2b
+# this is where the .fsarchive backup + restore scripts live, pass as arg
+d2b="$1"
+cd "$d2b"
 
 ls -alh
 pwd
 
-echo "Writing from $d2b... to $HOME PK:"
+echo "$(date) - Writing from $d2b to $HOME PK:"
 #read
 
-hdiutil makehybrid -verbose -iso -joliet -udf -print-size $d2b
+hdiutil makehybrid -verbose -iso -joliet -udf -print-size "$d2b"
 echo "Sawright?"
-read -n 1
+#read -n 1
 
-time hdiutil makehybrid -verbose -iso -joliet -udf -o $HOME/restoreUDF.iso $d2b
+time hdiutil makehybrid -verbose -iso -joliet -udf -o $HOME/restoreUDF.iso "$d2b"
 chmod a+rx $HOME/restoreUDF.iso
 
 ls -lh $HOME/restoreUDF.iso
@@ -42,10 +42,7 @@ read -n 1
 time \
   mkisofs -iso-level 3 -allow-lowercase -d -D -f -J -joliet-long -max-iso9660-filenames -N -r -T -udf -v \
   -o $HOME/restoreUDF.iso *
-#  | cdrecord -v -tao fs=5120k -eject speed=32 -data -
-#  | cdrecord -v -tao fs=5120k -eject speed=$CDR_SPEED -data -
 
-#dev=1,0,0
 chown dave /home/restoreUDF.iso
 chmod a+rx /home/restoreUDF.iso
 
