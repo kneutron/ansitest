220194d5b10cf1161974b1d5f851da7759e970fb
kneutron <50146127+kneutron@users.noreply.github.com>
2021-04-09 06:18:36 -0500

    wrong dir

--- vb-change-HD-uuid.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# REF: https://stackoverflow.com/questions/44114854/virtualbox-cannot-register-the-hard-disk-already-exists
-# use $PWD/name.vdi if needed
-VBoxManage internalcommands sethduuid "$1"
