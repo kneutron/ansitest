a087918cd19cdde33bd79818c460c1bc74456692
kneutron <50146127+kneutron@users.noreply.github.com>
2021-04-09 06:15:37 -0500

    Update vbox-wheredoesvmlive.sh

--- VIRTBOX/vbox-wheredoesvmlive.sh
+++ VIRTBOX/vbox-wheredoesvmlive.sh
@@ -1,3 +1,4 @@
-#!/bin/bash5
+#!/bin/bash
 
 VBoxManage list --long -s vms |egrep 'Name:|Config file:' |egrep -v 'Snap|Filter' |paste - -
+# Note GUI setting = rt-click on right pane, General / Location √
