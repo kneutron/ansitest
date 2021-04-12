6971d66a08e9c7b4cedf4bf5cd6e9575248dd92c
kneutron <50146127+kneutron@users.noreply.github.com>
2021-04-12 15:11:36 -0500

    more descriptive

--- ZFS/mkzfsraid10.sh
+++ ZFS/mkzfsraid10.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-pname=zsgtera4compr
+# Example create 4-drive mirror pool, create and chown a samba shared dataset and associated userid for it
+# userid needs to exist (useradd/adduser) first
 
-zpool create -f -o ashift=12 -o autoexpand=on -O atime=off -O compression=lz4 $pname \
+zp=zsgtera4compr
+
+zpool create -f -o ashift=12 -o autoexpand=on -O atime=off -O compression=lz4 $zp \
   mirror ata-ST4000VN000-1H4168_Z3073Z29 ata-ST4000VN000-1H4168_Z306G0K3 \
   mirror ata-ST4000VN000-1H4168_Z3073ZAY ata-ST4000VN000-1H4168_Z306G7H8
     
-pdir=tkita; zfs create -o atime=off -o sharesmb=on $pname/$pdir; chown tkita:dave /$pname/$pdir
+pdir=dave; zfs create -o atime=off -o sharesmb=on $zp/$pdir; chown $pdir:dave /$zp/$pdir
 ls -al /$pname/$pdir
     
 zpool status
     
 (set -r
-smbpasswd -a tkita
+smbpasswd -a $pdir
 )
     
