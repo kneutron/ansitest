84896313d836346a0283b125f2d4b341fb7facd0
kneutron <50146127+kneutron@users.noreply.github.com>
2021-04-12 15:04:54 -0500

    mod 4 standalone, depends

--- ZFS/mkgpt.sh
+++ ZFS/mkgpt.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# DEPENDS: smartctl, parted, fdisk
+
 argg=$1
 
-source ~/bin/failexit.mrg
+#source ~/bin/failexit.mrg
+# failexit.mrg
+function failexit () {
+  echo '! Something failed! Code: '"$1 $2" # code # (and optional description)
+  exit $1
+}
+
 
 smartctl -a /dev/$argg |head -n 16
 fdisk -l /dev/$argg
 
-ls -l /dev/disk/by-id|grep $argg
+ls -l /dev/disk/by-id |grep $argg
 
 echo "THIS WILL DESTRUCTIVELY APPLY A GPT LABEL - ARE YOU SURE - PK OR ^C"
 read
