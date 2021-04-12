67ab696a7ab799bb277fb3bcbb845b5d5a35949c
kneutron <50146127+kneutron@users.noreply.github.com>
2021-04-12 14:54:27 -0500

    Update mkdynamic-grow-pool-boojum.sh

--- ZFS/mkdynamic-grow-pool-boojum.sh
+++ ZFS/mkdynamic-grow-pool-boojum.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 #====================================================
-
-# =LLC= © (C)opyright 2016 Boojum Consulting LLC / Dave Bechtel, All rights reserved.
-## NOTICE: Only Boojum Consulting LLC personnel may use or redistribute this code,
-## Unless given explicit permission by the author - see http://www.boojumconsultingsa.com
-#
+# 2016 Dave Bechtel
 #====================================================
 
-# make a dynamic-growing zfs pool from 1 disk to 6
+# Interactive demo: make a dynamic-growing zfs pool from 1 disk to 6
 # zfs pools are on loop files (virtual disks)
+# NOTE this demo waits for multiple press-a-key-to-proceed prompts
 
 # trace on: set -x // off: set +x  # REF: http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_02_03.html
-# PROTIP: Avoid having to turn off and avoid clutter by using subshell REF: http://serverfault.com/questions/16204/how-to-make-bash-scripts-print-out-every-command-before-it-executes
+# PROTIP: Avoid having to turn off and avoid clutter by using subshell 
+# REF: http://serverfault.com/questions/16204/how-to-make-bash-scripts-print-out-every-command-before-it-executes
 
 # TODO setme or pass "new" as arg1
 newdisks=1  # set 1 to zero-out new disk files
@@ -21,15 +19,16 @@ newdisks=1  # set 1 to zero-out new disk files
 zdpath="/run/shm/zdisks"
 
 # if milterausb3 is mounted, use it
-usemil=`df |grep -c /mnt/milterausb3` 
+usemil=$(df |grep -c /mnt/milterausb3)
 [ $usemil -gt 0 ] && zdpath="/mnt/milterausb3/zdisks"
+# xxx TODO EDITME
 
 mkdir -pv $zdpath
 ln $zdpath /zdisks -sfn
 cd /zdisks || exit 99
 
 DS=400  # Disksize in MB
-[ $usemil -gt 0 ] && DS=465 # sim 500GB "usable"  # 500
+[ $usemil -gt 0 ] && DS=465 
 
 let mkpop=$DS-100  # size of populate-data file in MB
 
@@ -52,7 +51,7 @@ pool1="zdynpool1"
 
 nomorepool $pool1
 
-df -h
+df -hT
 zpool status $pool1
 echo "POOL $pool1 SHOULD BE GONE -PK"
 
@@ -61,7 +60,7 @@ echo "POOL $pool1 SHOULD BE GONE -PK"
 [ "$1" = "new" ] && let newdisks=1  # if arg passed
 
 [ $newdisks -gt 0 ] && read -n 1
-[ $newdisks -gt 0 ] && /root/bin/clearcache
+[ $newdisks -gt 0 ] && clearcache.sh
 
 # getting rid of sync for each dd should speed things up
 echo "Preparing virtual disks... (Size: $DS MB)"
@@ -77,6 +76,7 @@ done
 
 echo 'Syncing...'
 time sync
+# NOTE may not want to do this if you have a long-running I/O process (tar) because the sync will hang here
 
 ls -alh |tee -a $logfile
 
@@ -108,7 +108,7 @@ echo '';printf 'PK to add mirror to single disk:';read -n 1
   /zdisks/zdyndisk1 /zdisks/zdyndisk2) 
   
 echo 'o Should now have a 2-disk, MIRRORED pool with RAID1:'
-zpool status $pool1; df -h /$pool1
+zpool status $pool1; df -hT /$pool1
 echo '';echo 'o NOTE that available pool space has not changed yet - we have only added Redundancy'
 echo '! NORMALLY we would wait for the resilver to complete before proceeding! # zpool status  # until resilvered'
 printf 'PK to add another set of mirrored disks to the existing pool:';read -n 1
@@ -124,7 +124,7 @@ echo 'o Populating pool with more data...'
 time cp -v /$pool1/tmpfile /$pool1/tmpfile2    
     
 echo 'o Should now have a 4-disk, redundant pool with RAID10:'
-zpool status $pool1; df -h /$pool1
+zpool status $pool1; df -hT /$pool1
 ls -lh /$pool1/*
 
 echo '';echo 'o NOTE that the available pool space should be approximately 2x what we had before, minus a bit of overhead'
@@ -142,7 +142,7 @@ printf 'PK to add final 2-disk mirror with 1 hotspare:';read -n 1
 
 
 echo 'o Should now have a 6-disk, highly failure-resistant pool with RAID10:'
-zpool status $pool1; df -h /$pool1; zpool list $pool1
+zpool status $pool1; df -hT /$pool1; zpool list $pool1
 echo ''
 echo 'o NOTE that the available pool space should be approximately 1.5x what we had with the 4-disk mirror set,'
 echo ' minus a bit of overhead'
@@ -151,7 +151,7 @@ printf 'PK to proceed with next phase (POOL WILL BE DESTROYED and rebuilt):';rea
 
 
 
-####################### start over and build pool from 2-disk RAID0
+####################### start over and build redundant RAID10 pool from 2-disk RAID0
 
 nomorepool $pool1
 
@@ -170,10 +170,10 @@ time cp -v /root/tmpfile /$pool1
 time cp -v /$pool1/tmpfile /$pool1/tmpfile2  
   
 echo 'o OK, say our client has a limited budget and wants to start with 2 disks with maximum writable space, NO redundancy... (RAID0)'
-zpool status $pool1; df -h /$pool1
+zpool status $pool1; df -hT /$pool1
 echo '';echo 'o NOTE that the available pool space should be about the capacity of disk1+disk2 minus a bit of overhead;'
 echo '+ it will be fast, but vulnerable to failure. NOTE that we will have done a *full burn-in test* on the drives 1st!'
-echo 'o After a couple of weeks, client is able to buy 2 more drives -- we can both Expand the pool size AND add redundancy!'
+echo 'o After a couple of weeks, client is able to buy 2 more drives -- we can add redundancy!'
 printf 'PK to add a mirror to 1st disk:';read -n 1
 
 
@@ -184,7 +184,7 @@ printf 'PK to add a mirror to 1st disk:';read -n 1
    /zdisks/zdyndisk1 /zdisks/zdyndisk3)
 
 echo 'o Should now have a 3-disk, UNBALANCED MIRRORED pool with half-RAID1:'
-zpool status $pool1; df -h /$pool1
+zpool status $pool1; df -hT /$pool1
 echo '';echo 'o NOTE that available pool space has **not changed** - we have only added Redundancy to HALF of the pool!'
 echo '! NORMALLY we would NEED to wait for the resilver to complete before proceeding! # zpool status  # until resilvered'
 printf 'PK to add a mirror to the 2nd half (will end up being RAID10):';read -n 1
@@ -200,7 +200,7 @@ echo 'o Populating pool with more data...'
 time cp -v /$pool1/tmpfile /$pool1/tmpfile2    
   
 echo 'o Should now have a 4-disk, redundant pool with RAID10 - but we built it differently since it started out as a RAID0:'
-zpool status $pool1; df -h /$pool1
+zpool status $pool1; df -hT /$pool1
 
 echo '';echo 'o NOTE that the available pool space is still the same as what we started with,'
 echo '+ only now the pool has been upgraded in-place to be failure-resistant - with no downtime!'
@@ -208,7 +208,7 @@ echo '! Again - NORMALLY we would wait for the resilver to complete before proce
 printf 'PK to expand this now-RAID10 pool with the final 2-disk mirror, plus 1 hotspare:';read -n 1
 
 
-########### add final 2-disk mirror to 4-disk RAID10, with 1 spare
+########### Expand free space = add final 2-disk mirror to 4-disk RAID10, with 1 spare
 # REF: http://docs.oracle.com/cd/E19253-01/819-5461/6n7ht6qvk/index.html
 (set -x
  time zpool add -o ashift=12 $pool1 \
@@ -224,7 +224,7 @@ echo ' with some acceptable risks but built on a budget:'
 # ENDIT
 zpool status -L $pool1
 #df -h |head -n 1 # unnec; fortuitously, grep also grabs "size"
-df -h |grep z 	# /$pool1 /$pool2 /$pool3a /$pool3b /$pool3c
+df -hT |grep z 	# /$pool1 /$pool2 /$pool3a /$pool3b /$pool3c
 (set -x; zfs list $pool1; zpool list $pool1)
 echo ''
 echo "Log: $logfile"
@@ -238,11 +238,7 @@ zpool status
 exit;
 
 ====================================================
-
-# =LLC= © (C)opyright 2016 Boojum Consulting LLC / Dave Bechtel, All rights reserved.
-## NOTICE: Only Boojum Consulting LLC personnel may use or redistribute this code,
-## Unless given explicit permission by the author - see http://www.boojumconsultingsa.com
-#
+# 2016 Dave Bechtel
 ====================================================
 
 # df -h /zdynpool1
@@ -254,7 +250,6 @@ zdynpool1       832M     0  832M   0% /zdynpool1
  state: ONLINE
   scan: resilvered 276K in 0h0m with 0 errors on Mon May  9 16:49:45 2016
 config:
-
         NAME                   STATE     READ WRITE CKSUM
         zdynpool1              ONLINE       0     0     0
           mirror-0             ONLINE       0     0     0
@@ -269,7 +264,6 @@ config:
         spares
           /zdisks/zdyndisk7    AVAIL
 errors: No known data errors
-
 
 ========================================================
 
