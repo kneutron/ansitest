6afe0c9fdfc199fcf24d61ffb0cbfa134b565e3f
kneutron <50146127+kneutron@users.noreply.github.com>
2021-05-31 11:12:44 -0500

    Add files via upload

---
+++ ZFS/zfs-1week-snapshot.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# 2014-2021 Dave Bechtel
+# this runs in cron.daily
+# and keeps 7 days of rotating snapshots
+# + plus 28-31 days of rotating snapshots, depending on days in month
+
+logfile=/root/zfs-1week-snapshot.log
+snaplog=/root/zfs-snaplist-all.log
+
+/bin/mv -vf $snaplog $snaplog-old
+
+PATH=/sbin:/root/bin:/root/bin/zfs:/usr/local/bin:/usr/local/sbin:/usr/sbin:/bin:/usr/bin:/usr/X11R6/bin: #/usr/NX/bin:/usr/games:
+echo "=== START RUN $(date) - zfs 1-week +28-31 day snapshot" >> $logfile
+
+# speed things up a bit
+#/root/bin/zfs/spinup 2>&1 >> $logfile
+
+myday=$(date +%a) # ex. Sun  
+mydnum=$(date +%d) # ex. 04  
+
+#echo "= BEGIN Stage DF before, snaps before" >> $logfile
+echo "= BEGIN Stage 1: DF before" >> $logfile
+df -h -T |grep zfs >> $logfile
+#zfs list -H -t snapshot |column -t >> $logfile
+
+  
+for i in $(zpool list |grep -v ALLOC |awk '{ print $1 }');do
+  echo "= INFO $i:$myday:$mydnum" >> $logfile
+
+# kill ANY preexisting snapshot with this daylabel
+  echo "= BEGIN Stage 2: Destroy existing snaps for pool $i" >> $logfile
+
+  zfs destroy -R -v $i@$myday 2>&1 >>$logfile;rc=$?
+  echo "= DESTROY $i@$myday RTN CODE:$rc" >> $logfile
+
+  zfs destroy -R -v $i@zfsDOM$mydnum 2>&1 >>$logfile;rc=$?
+  echo "= DESTROY $i@zfsDOM$mydnum RTN CODE:$rc" >> $logfile
+
+# this is for 28-31 days, uncomment if needed beyond 1 week
+#  zfs snapshot -r $i@zfsDOM$mydnum
+  zfs snapshot -r $i@$myday
+done
+
+echo "= BEGIN Stage 3: Today Snaps after, DF after" >> $logfile        
+
+#zfs list -H -t snapshot |grep $myday >> $logfile
+#zfs list -H -t snapshot |grep $mydnum >> $logfile
+
+# ALL snaps here
+#zfs list -H -t snapshot |column -t > $snaplog
+zfs-list-snaps--boojum.sh |column -t > $snaplog
+
+# Only today+wkly here
+#zfs-list-snaps--boojum.sh |egrep "@$myday|DOM$mydnum|weekly" |column -t >> $logfile
+
+df -h -T |grep zfs >> $logfile
+echo "=== END RUN zfs 1wk/28-31 day snapshot: `date`" >> $logfile 
+  
+# DONE while we're here, keep 28-31 day number backups too
+
+exit 0;
+
+
+2017.1228 switched make-snapshot order to have ex. "Wed" after DOM to avoid snapshot rollback "newer exists"
++ fixed df -h -T |grep zfs since others may not have zfs filesystems starting w/Z
+
+TODO log rotation 1/mo
+
+2017.1229 DONE zfs-list-snaps instead (shows creation date
+DONE save space in log - when listing snapshots, only list relevent-day
+DONE enumerated stages better (for searching
