784ac8dfa20a7bf2f64e9bc8375765a54f0e8b94
kneutron <50146127+kneutron@users.noreply.github.com>
2021-06-03 20:26:13 -0500

    Update zfs-killmonth-snaps.sh

--- ZFS/zfs-killmonth-snaps.sh
+++ ZFS/zfs-killmonth-snaps.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+# Tested with Ubuntu
 # 2020.1005 mod for auto-month
+# 2014-2021 Dave Bechtel
 
 # test for interactive shell / OK to ask for input
 intera=1
-[ `echo $- |grep i |wc -l` -gt 0 ] && intera=0
+[ $(echo $- |grep i |wc -l) -gt 0 ] && intera=0
 
 # TODO comment if you want auto
 #mymonth=Mar
 outfile=/tmp/zfs-killmonth-tmp.txt
 
-source ~/bin/failexit.mrg
+#source ~/bin/failexit.mrg
+# failexit.mrg
+function failexit () {
+  echo '! Something failed! Code: '"$1 $2" # code # (and optional description)
+  exit $1
+}
 
 #zfs-list-snaps--boojum.sh |grep virtb |grep Jan |awk '{print $1}' |xargs -n1 -t zfs-killsnaps.sh
 
@@ -27,18 +34,19 @@ pmonth=$(date --date="$(date +%Y-%m-15) -2 month" |awk '{print $2}')
 
 # sanity check for valid month
 validmo="Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec"
-sanity=`echo $validmo |grep -c $mymonth` # will NOT allow "July", only 3-letter
+sanity=$(echo $validmo |grep -c $mymonth) # will NOT allow "July", only 3-letter
 
 if [ $sanity -eq 0 ]; then
   failexit 12 "! Invalid month specified: $mymonth"
 fi
 
-echo "`date` - DF before:" > $outfile
+echo "$(date) - DF before:" > $outfile
 df -h -T |head -n 1 >> $outfile
 df -h -T |grep zfs >> $outfile
 
 # could use more error checking if $mymonth = 0 lines...
-numlines=`zfs-list-snaps--boojum.sh |grep $mymonth |wc -l`
+# NOTE requires another script in PATH
+numlines=$(zfs-list-snaps--boojum.sh |grep $mymonth |wc -l)
 if [ $numlines -gt 0 ]; then
   echo "o KILLING THESE SNAPSHOTS:" >> $outfile
   zfs-list-snaps--boojum.sh |grep $mymonth >> $outfile # full dump of snapshots to delete
@@ -49,13 +57,13 @@ else
   failexit 101 "No $mymonth snapshots found to kill!"
 fi
 
-# killzem
+# killzem - NOTE requires another script in PATH
 zfs-list-snaps--boojum.sh |grep $mymonth |awk '{print $1}' |xargs -n1 -t zfs-killsnaps.sh
 
 # review
 [ $intera -gt 0 ] && zfs-list-snaps--boojum.sh |less
 
-echo "`date` - DF after:" >> $outfile
+echo "$(date) - DF after:" >> $outfile
 #df -h >> $outfile
 df -h -T |head -n 1 >> $outfile
 df -h -T |grep zfs >> $outfile
