d8fbbefc085d3fe94a31b1a4e5fdb57765f25de8
kneutron <50146127+kneutron@users.noreply.github.com>
2021-07-19 08:47:32 -0500

    Update ssh-into-qemu-vm.sh

--- ZFS/ssh-into-qemu-vm.sh
+++ ZFS/ssh-into-qemu-vm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# portfwd to squid on pi; run-qemu is listening for ssh on 32222
+# This runs on the QEMU Host
+# portfwd to squid on raspberry pi; run-qemu is listening for ssh on 32222
 ssh -2 -C -Y -oTCPKeepAlive=yes -g -R 3128:10.1.0.4:3128 -l user localhost -p 32222
