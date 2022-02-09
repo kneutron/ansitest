f10efd0c18a15ed6799140d59caa5df15a42bfe3
kneutron <50146127+kneutron@users.noreply.github.com>
2022-02-08 20:52:01 -0600

    Add files via upload

---
+++ winstuff/noscreensaver.bat
@@ -0,0 +1,10 @@
+REM @echo off
+powercfg.exe -x -monitor-timeout-ac 0
+powercfg.exe -x -monitor-timeout-dc 0
+powercfg.exe -x -disk-timeout-ac 0
+powercfg.exe -x -disk-timeout-dc 0
+powercfg.exe -x -standby-timeout-ac 0
+powercfg.exe -x -standby-timeout-dc 0
+powercfg.exe -x -hibernate-timeout-ac 0
+powercfg.exe -x -hibernate-timeout-dc 0
+pause
---
+++ winstuff/win10-upgrade-switches.cmd
@@ -0,0 +1,2 @@
+@echo off
+J:\setup /Auto Upgrade /CompactOS Enable /DynamicUpdate disable /EULA accept /MigNEO disable /ShowOOBE none /Telemetry disable /Tempdrive D /Uninstall disable 
