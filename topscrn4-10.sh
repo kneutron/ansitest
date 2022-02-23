4f791e2cf71468df04ac7dab39ff8ca6627dac0c
kneutron <50146127+kneutron@users.noreply.github.com>
2022-02-22 20:16:45 -0600

    Update topscrn4-10.sh

--- topscrn4-10.sh
+++ topscrn4-10.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-ff # fix fontsize
+#ff # fix fontsize
+
+# Requires: bwm-ng top screen iostat watch openvt/open
 
 useprog=open
 [ -e `which openvt` ] && useprog=openvt
 
-# open 4-pane screen on vt10
+# turn off screensaver
 setterm -blank 0 2>/dev/null
+
+# open 4-pane screen on vt10 and switch to it
 ( TERM=linux $useprog -f -c 10 -s -w -- /usr/bin/screen -S topscrn410 -c /root/.screenrc-mon1-combined ) &
 disown -a
