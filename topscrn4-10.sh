c1903704e1df47d109cf697755e1fd05f944d661
kneutron <50146127+kneutron@users.noreply.github.com>
2022-02-22 20:38:16 -0600

    Update topscrn4-10.sh

--- topscrn4-10.sh
+++ topscrn4-10.sh
@@ -3,6 +3,8 @@
 #ff # fix fontsize
 
 # Requires: bwm-ng top screen iostat watch openvt/open
+# Requires other files: dot-screenrc-mon1-combined ( renamed as .screenrc-mon1-combined in root's home dir )
+# ^^ mon1-df-short in /usr/local/bin or $HOME/bin
 
 useprog=open
 [ -e `which openvt` ] && useprog=openvt
