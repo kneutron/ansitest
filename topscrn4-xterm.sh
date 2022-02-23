a70915e0e221f0479d0c5babf0b66e507b5754c2
kneutron <50146127+kneutron@users.noreply.github.com>
2022-02-22 21:40:14 -0600

    Update topscrn4-xterm.sh

--- topscrn4-xterm.sh
+++ topscrn4-xterm.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# This is useful with Maximized lxterminal, xfce4-terminal, etc where you can easily resize the font
-# if using ssh Use: ssh -t localhost /$HOME/bin/topscrn4-xterm.sh
+# This is useful with Maximized lxterminal, xfce4-terminal, OSX terminal, etc where you can easily resize the font
+# NOTE: if using ssh Use: ssh -t hostname $HOME/bin/topscrn4-xterm.sh
 
 # open 4-pane screen with sys monitoring
 
 # Requires: bwm-ng top screen iostat watch openvt/open
+
 # Requires other files: dot-screenrc-mon1-combined ( renamed as .screenrc-mon1-combined in user's home dir )
 # ^^ mon1-df-short in /usr/local/bin 
 
