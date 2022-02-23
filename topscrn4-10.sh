aa5ceb7f36cbec81ed5583d99d2577734b136d2f
kneutron <50146127+kneutron@users.noreply.github.com>
2022-02-22 20:13:23 -0600

    Update topscrn4-10.sh

--- topscrn4-10.sh
+++ topscrn4-10.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-ff
+ff # fix fontsize
 
 useprog=open
 [ -e `which openvt` ] && useprog=openvt
+
 # open 4-pane screen on vt10
 setterm -blank 0 2>/dev/null
 ( TERM=linux $useprog -f -c 10 -s -w -- /usr/bin/screen -S topscrn410 -c /root/.screenrc-mon1-combined ) &
+disown -a
