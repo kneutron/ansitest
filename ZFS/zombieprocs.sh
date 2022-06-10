61148b40d749a993b72da689820812ae88a40116
kneutron <50146127+kneutron@users.noreply.github.com>
2022-06-10 13:44:13 -0500

    Delete zombieprocs.sh

--- ZFS/zombieprocs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Linux ps
-ps ax -o pid,ppid,s,cmd |awk '$3 ~ /Z/'
-# ^ Print pid, parent pid, State, commandline and only print if 3rd column matches Z = zombie
-
-# kill -9 on the PARENT pid, should clear them up
