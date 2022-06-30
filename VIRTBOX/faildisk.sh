96363142a493745f9e48b2d0fddb28e4707334cd
kneutron <50146127+kneutron@users.noreply.github.com>
2022-06-30 11:09:30 -0500

    Update faildisk.sh

--- VIRTBOX/faildisk.sh
+++ VIRTBOX/faildisk.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # arg = shortname disk e.g. sdc , omit the /dev
 
+# Primarily used in ZFS VM to simulate disk failures in a zpool / test RAIDZx / DRAID recovery scenarios
+# NOTE rebooting will fix the failed disks
+
 echo "Failing $1"
 echo offline > /sys/block/$1/device/state 
 echo 1 > /sys/block/$1/device/delete 
