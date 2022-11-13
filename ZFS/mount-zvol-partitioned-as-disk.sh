3be3d8b79e989261c1052ea3c84d77b94716b31c
kneutron <50146127+kneutron@users.noreply.github.com>
2022-11-13 08:34:39 -0600

    better commenting and vars

--- ZFS/mount-zvol-partitioned-as-disk.sh
+++ ZFS/mount-zvol-partitioned-as-disk.sh
@@ -4,21 +4,27 @@
 
 exit;
 
+# This is for when you have a 64GB 'dd' backup and want to access the partitions without restoring to original media
 
-zfs create -sV 64g zseatera2/zvol01
+zp=zseatera2
+size=64 # GB
+zfs create -sV "$size"g $zp/zvol01
 
-fdisk -l /dev/zvol/zseatera2/zvol01
+fdisk -l /dev/zvol/$zp/zvol01
  
 # mount /dev/zvol/zseatera2/zvol01p5 /mnt/tmp
 #mount: special device /dev/zvol/zseatera2/zvol01p5 does not exist
 
 #FIX: REF: https://www.golinuxcloud.com/losetup-command-in-linux/#1_Create_a_loop_device_with_losetup
-losetup -P -f /dev/zvol/zseatera2/zvol01      # need -P for partitions
+losetup -P -f /dev/zvol/$zp/zvol01      # need -P for partitions
 
 losetup
 #NAME       SIZELIMIT OFFSET AUTOCLEAR RO BACK-FILE DIO
 #/dev/loop0         0      0         0  0 /dev/zd0    0
 
-fdisk -l /dev/loop0
 
-mount /dev/loop0p5 /mnt/tmp -onoatime
+mydev=$(losetup |grep zd0 |awk '{print $1}')
+fdisk -l $mydev   # /dev/loop0
+
+mkdir -pv /mnt/tmp
+mount /dev/"$mydev"p5 /mnt/tmp -oro # rw,noatime
