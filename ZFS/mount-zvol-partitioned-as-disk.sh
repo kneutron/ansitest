1768fb5faa9783c472d67f0f1f34d3cb111aeda7
kneutron <50146127+kneutron@users.noreply.github.com>
2022-11-13 08:40:51 -0600

    more commenting and actual howto

--- ZFS/mount-zvol-partitioned-as-disk.sh
+++ ZFS/mount-zvol-partitioned-as-disk.sh
@@ -5,13 +5,19 @@
 exit;
 
 # This is for when you have a 64GB 'dd' backup and want to access the partitions without restoring to original media
+# dd if=/dev/sdXX of=dd-pny-64gig-b4-reimage-with-memtest-20221108.dd bs=1M
 
 zp=zseatera2
 size=64 # GB
 zfs create -sV "$size"g $zp/zvol01
 
+# restore to compressed zvol (compression is inherited from pool level but can be set OTF)
+# zfs set compression=zstd-3 $zp/zvol01
+# time dd if=dd-pny-64gig-b4-reimage-with-memtest-20221108.dd of=/dev/zvol/$zp/zvol01 bs=1M
+
 fdisk -l /dev/zvol/$zp/zvol01
- 
+
+# ISSUE:
 # mount /dev/zvol/zseatera2/zvol01p5 /mnt/tmp
 #mount: special device /dev/zvol/zseatera2/zvol01p5 does not exist
 
@@ -22,7 +28,7 @@ losetup
 #NAME       SIZELIMIT OFFSET AUTOCLEAR RO BACK-FILE DIO
 #/dev/loop0         0      0         0  0 /dev/zd0    0
 
-
+# TODO grep ls /dev/zvol/$zp/zvol01 for the right one, its a symlink
 mydev=$(losetup |grep zd0 |awk '{print $1}')
 fdisk -l $mydev   # /dev/loop0
 
