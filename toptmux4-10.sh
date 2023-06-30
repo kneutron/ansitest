7a2d146177bcd2bed6980075980197705a7db8bd
kneutron <50146127+kneutron@users.noreply.github.com>
2023-06-30 14:41:27 -0500

    Update toptmux4-10.sh

--- toptmux4-10.sh
+++ toptmux4-10.sh
@@ -7,12 +7,14 @@ useprog=open
 # open 4-pane screen on vt10
 setterm -blank 0 2>/dev/null
 
+uset=$(which tmux)
+
 #( TERM=linux $useprog -f -c 10 -s -w -- /usr/bin/tmux -S topscrn410 -c /root/.screenrc-mon1-combined ) &
-( TERM=linux $useprog -f -c 10 -s -w -- /opt/local/bin/tmux -S topTM410 tmux new-session \; \
+( TERM=linux $useprog -f -c 10 -s -w -- $uset -S topTM410 tmux new-session \; \
   send-keys 'top -d15' C-m \; \
   split-window -v \; \
   send-keys 'bwm-ng -t 2000' C-m \; \
   split-window -h \; \
-  send-keys 'iostat -k 5' C-m \;
+  send-keys 'iostat -k -s -y -z 5' C-m \;
   split-window -h \; \
   send-keys 'watch -n 61 /home/dave/bin/mon1-df-short' ) &
