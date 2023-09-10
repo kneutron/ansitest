747e46db3d28602d79ef46ac2c6e4e26459fcc63
kneutron <50146127+kneutron@users.noreply.github.com>
2023-09-10 18:44:40 -0500

    Add files via upload
    check if already running

--- topterm9.sh
+++ topterm9.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+result=$(ps ax |awk '/tty9/ {print }' |grep -c top)
+
+if [ $result -eq 0 ]; then
 # open top on vt9
 setterm -blank 0 2>/dev/null
-
-usepg=open
-[ -e `which openvt` ] && usepg=openvt
-
-( TERM=linux $usepg -f -c 9 -s -w -- /usr/bin/top -d 15 ) &
+( TERM=linux openvt -f -c 9 -w -- /usr/bin/top -d 15 ) &
+else
+  echo "WARNING topterm9 already running - skipping"
+fi
+ 
