2654ea84442d4bbd9d01e72dd623db1b3cde3559
kneutron <50146127+kneutron@users.noreply.github.com>
2023-09-17 00:12:01 -0500

    Update chocolatey-install.cmd

--- winstuff/chocolatey-install.cmd
+++ winstuff/chocolatey-install.cmd
@@ -1,2 +1,5 @@
+@echo RUN AS ADMIN
+pause
+
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 pause
