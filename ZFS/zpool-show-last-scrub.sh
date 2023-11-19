32938b00c9dfff243f76023098319a2d7308c7fb
kneutron <50146127+kneutron@users.noreply.github.com>
2023-11-19 13:50:39 -0700

    Update zpool-show-last-scrub.sh

--- ZFS/zpool-show-last-scrub.sh
+++ ZFS/zpool-show-last-scrub.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 zpool status -v |egrep 'pool:|scan' #|less
+
+exit;
+
+# sample output
+  pool: zint500
+  scan: scrub repaired 0B in 00:48:16 with 0 errors on Thu Nov 16 00:49:16 2023
+  pool: zsam53
+  scan: scrub repaired 0B in 01:42:37 with 0 errors on Wed Nov 15 01:43:37 2023
+  pool: ztoshtera6
+  scan: scrub repaired 0B in 22:32:04 with 0 errors on Fri Nov 17 22:33:05 2023
