e56eb30cd7afb465ac6cb70bd503b7697d29470a
kneutron <50146127+kneutron@users.noreply.github.com>
2024-02-15 09:37:01 -0700

    Update convert-vdi-disks-to-qcow2.sh

--- proxmox/convert-vdi-disks-to-qcow2.sh
+++ proxmox/convert-vdi-disks-to-qcow2.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# to convert existing virtualbox VDI disks to qcow2, for import into proxmox vm environment
+# 2024 kneutron
+
+# xxx TODO EDITME - this is the base dir for virtualbox / disk images
 based="/zseatera4-f4m8/virtbox-virtmachines"
 cd "$based" || exit 44;
 
+# xxx TODO EDITME - this is where converted disks .qcow2 will go
 dest=/mnt/seatera4-xfs
 
 for disk in $(find  -name *.vdi |sed 's|^./||'); do 
@@ -11,11 +16,11 @@ for disk in $(find  -name *.vdi |sed 's|^./||'); do
   
   diskn=$(basename "$disk")
   
-echo "dirn=$dirn"
-echo "disk=$disk"
-echo "diskn=$diskn"
+#echo "dirn=$dirn"
+#echo "disk=$disk"
+#echo "diskn=$diskn"
   
-  echo "$(date) - Convert $PWD $diskn" 
+  echo "$(date) - Converting $PWD $diskn" 
   time qemu-img convert -f vdi -O qcow2 "$PWD/$diskn" "$dest/$diskn.qcow2"
 #  cd "$based"
 done
