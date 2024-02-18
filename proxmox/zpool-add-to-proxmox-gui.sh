ed4736d21b8b3baaeb2b9949d624c41c63f820dc
kneutron <50146127+kneutron@users.noreply.github.com>
2024-02-18 09:55:34 -0700

    Update zpool-add-to-proxmox-gui.sh

--- proxmox/zpool-add-to-proxmox-gui.sh
+++ proxmox/zpool-add-to-proxmox-gui.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-exit;
+# this is for when the zfs pool was created / imported commandline and GUI does not detect it
 
-   pvesm add zfspool zfs2nvme -pool znvme
+# xxx TODO EDITME or use $1 / $2
+pvesm add zfspool zfs2nvme -pool znvme
 
-# zfs2nvme = proxmox name for gui
+# zfs2nvme = proxmox name you want displayed in gui
 # znvme = actual zpool name
  
