6529db012a8e0ba334746dfa45782b951f8fb504
kneutron <50146127+kneutron@users.noreply.github.com>
2024-02-19 11:54:45 -0700

    add arg override for newvm number

--- proxmox/newvm-proxmox.sh
+++ proxmox/newvm-proxmox.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Import converted vdi virtualbox disks to proxmox vm (Linux version)
-# 2024 kneutron
-
 source ~/bin/failexit.mrg
 
-# xxx TODO EDITME - converted qcows from vbox are here
+# converted qcows from vbox
 cd /mnt/seatera4-xfs
 
-echo "Usage: \$1=name-of-vm \$2=RAM-in-GB [optional]"
+echo "Usage: \$1=name-of-vm \$2=RAM-in-GB [optional] \$3=newvmnumber"
 
 if [ "$1" = "" ]; then
   failexit 101 "1st arg not supplied, must be VMNAME"
@@ -16,11 +13,11 @@ else
  newname="$1"
 fi
 
-#linux=l26
-#oldlinux=l24
+#linux=126
+#oldlinux=124
 
 # integer / number
-declare -i ram lastid newid
+declare -i ram lastid newid #newidarg
 
 if [ "$2" = "" ]; then
  ram=4096
@@ -39,30 +36,30 @@ date
 #       104 pfsense-272          running    1500              20.00 813250
  
 let newid=$lastid+1 
+[ "$3" = "" ] || newid=$3
 echo "Last vmid: $lastid - New id: $newid -- RAM: $ram -- PK"
 read -n 1       
        
 # --ostype l26 \
-# --ostype other \
+# --ostype win10 \
 # --boot order=scsi0 \
-# onboot = 1 means start thisvm at proxmox host boot
 qm create $newid \
- --net0 virtio,bridge=vmbr0,firewall=0,link_down=0,firewall=0 \
- --net1 virtio,bridge=vmbr25,firewall=0,link_down=0,firewall=0 \
- --net2 virtio,bridge=vmbr1,firewall=0,link_down=0,firewall=0 \
+ --net0 virtio,bridge=vmbr0,link_down=0,firewall=0 \
+ --net1 virtio,bridge=vmbr25,link_down=0,firewall=0 \
+ --net2 virtio,bridge=vmbr1,link_down=0,firewall=0 \
  --name "$newname" \
  --scsihw virtio-scsi-single \
- --cores 2 --sockets 1 --cpu cputype=host \
+ --cores 1 --sockets 1 --cpu cputype=host \
  --ide2 none,media=cdrom \
  --memory $ram \
  --vga=virtio,memory=128 \
  --balloon 2048 \
  --start 0 \
- --onboot 1 \
+ --onboot 0 \
  --boot order="ide2;sata0" \
- --sata0 zfs1:0,cache=writeback,import-from=$PWD/cubietruck-temp-replacement-squid-pihole-sata0-0.vdi.qcow2 \
- --sata1 zfs1:0,cache=writeback,import-from=$PWD/cubietruck-temp-replacement-squid-pihole-sata0-1-squidextend.vdi.qcow2 \
+ --sata0 zfs1:0,cache=writeback,import-from=$PWD/suse-tumbleweed-20240219-desktop-kde-disk0.qcow2 \
 || failexit 40 "Failed to create VM $newid $newname"
+# --sata1 zfs1:0,cache=writeback,import-from=$PWD/cubietruck-temp-replacement-squid-pihole-sata0-1-squidextend.vdi.qcow2 \
 
 qm list
 
@@ -70,3 +67,8 @@ exit;
 
 	  L, not One
 --ostype <l24 | l26 | other | solaris | w2k | w2k3 | w2k8 | win10 | win11 | win7 | win8 | wvista | wxp>
+
+400 Parameter verification failed.  
+ostype: value '126' does not have a
+value in the enumeration 'other, wxp, w2k, w2k3, w2k8, wvista, win7, win8,
+win10, win11, l24, l26, solaris'
