bc93a2354592d5ac3de2e6723fc96480f5ccaa87
kneutron <50146127+kneutron@users.noreply.github.com>
2024-02-25 18:33:54 -0700

    Add files via upload

--- proxmox/proxmox-query-vm-agents.sh
+++ proxmox/proxmox-query-vm-agents.sh
@@ -3,11 +3,17 @@
 # 2024.feb kneutron
 # REF: https://youtu.be/wp4kCUM6dik?t=233
 
-time qm list 
+time qm list |column -t
 
 for vmid in $(qm list |grep running |awk '{print $1}'); do
   echo "$(date) - Checking $vmid"
-  qm agent $vmid ping || echo "$vmid guest agent not responding!"
+  qm agent $vmid ping; rc=$?
+  if [ $rc -gt 0 ]; then
+    echo "$vmid guest agent not responding!"
+    continue;
+  fi
+  qm guest cmd $vmid get-host-name |tr -d '{}' |awk 'NF>0'
+#  qm guest cmd $vmid get-osinfo
 done
 
 exit;
