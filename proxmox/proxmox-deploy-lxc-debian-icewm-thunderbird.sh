6ebffff8c1ca12a4aa1f4bba2bf0ad448b8f172b
kneutron <50146127+kneutron@users.noreply.github.com>
2024-12-11 19:34:56 -0700

    V2

--- proxmox/proxmox-deploy-lxc-debian-icewm-thunderbird.sh
+++ proxmox/proxmox-deploy-lxc-debian-icewm-thunderbird.sh
@@ -2,14 +2,17 @@
 
 # 2024.Dec kneutron
 
+# This is script 2 of 2 - run the below one 1st to create the LXC
+# https://github.com/kneutron/ansitest/blob/master/proxmox/proxmox-create-lxc-xrdp-icewm-thunderbird-debian.sh
+
+# Goal: Setup Unprivileged LXC with Debian Standard 12, 2xvCPU (you can probably get away with 1), rootfs 10GB, RAM 1GB, 512MB Swap, IP = DHCP or static, DNS as appropriate
+
 # REF: https://www.reddit.com/r/Proxmox/comments/1hayp1j/comment/m1ilo4n/?context=3
 
-# Setup Unprivileged LXC with Debian Standard 12, 2xvCPU (you can probably get away with 1), rootfs 10GB, RAM 2GB, 512MB Swap, IP = DHCP or static, DNS as appropriate
 
-# Fire up LXC console after it builds, login as root with the password you set
+# Fire up LXC console after it builds, root login should already be there, no password needed: #
 
-# Run this script INSIDE the running LXC
-# TODO 2nd script for pct create
+# Run this script INSIDE the running LXC, you may need to ' chmod +x ' it first
 
 # (root) Cmds to accomplish everything:
 
@@ -17,14 +20,11 @@
 myid=dave
 
 apt update
-
 apt upgrade -y
 
 # SKIP #  apt install fluxbox # rdesktop clicking did nothing, so went with icewm
 
 apt install -y icewm xterm xfce4-terminal
-
-ip a
 
 apt install -y xrdp
 
@@ -38,14 +38,20 @@ apt install -y thunderbird
 
 # Requires input for passwd
 adduser $myid
+echo ''
 
 #screen -aAO -h 2000
 
 # =============
 
-echo "Fire up rdesktop client, login as $myid, you should see icewm minimal desktop with 4 virtual screens preconfigured."
+echo "Fire up rdesktop client / mstsc, point it to the below IP address, login as $myid"
+echo "  You should see icewm minimal desktop with 4 virtual screens preconfigured."
 echo ''
-echo "Right-click desktop, open Terminal, enter ' thunderbird & '"
-echo '(or use the "Start" menu) and setup your email account.'
+echo "Right-click the desktop, open Terminal, enter ' thunderbird & '"
+echo '(or use the "Start" menu) and setup your email account(s).'
+
+ip a
 
 date;
+
+# NOTE you can install e.g. fluxbox afterward to save RAM, and switching to it from icewm should work
