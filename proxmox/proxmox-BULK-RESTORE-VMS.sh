e9ab326e9fda28c75ea1ec72773fceeca7b0ee57
kneutron <50146127+kneutron@users.noreply.github.com>
2025-02-07 09:13:26 -0700

    V2
    pct restore needed new method

--- proxmox/proxmox-BULK-RESTORE-VMS.sh
+++ proxmox/proxmox-BULK-RESTORE-VMS.sh
@@ -8,8 +8,11 @@
 # NOTE - THIS SCRIPT REQUIRES INPUT TO PROCEED
 
 # xxx TODO EDITME
-TESTONLY=1
 # set to 0 for actual restore, 1 if you want to run thru script WITHOUT restore step (dry run)
+TESTONLY=1
+
+restorelxc=1 # set to 0 to not restore LXC
+restorevm=1
 
 # (get name from PVE GUI or ' pvesm status ')
 storage=pbsvm3-25g-vm-on-beelink
@@ -19,6 +22,7 @@ dest="xfs-exos10"
 # destination storage
 
 
+# ========================
 # No more edits after this
 outfile=/dev/shm/pvemstorage.list
 restorethese=/dev/shm/bkps2restore.list
@@ -27,6 +31,7 @@ rm -fv "$restorethese" 2>/dev/null
 logf=/dev/shm/BULK-RESTORE-VMS.log
 /bin/mv -v "$logf" "$logf--old" 2>/dev/null
 
+clear
 echo ''
 echo "-- YOU NEED TO EDIT THIS SCRIPT BEFORE RUNNING IT --"
 echo "SCRIPT AUTHOR TAKES NO RESPONSIBILITY FOR DATA LOSS"
@@ -44,11 +49,18 @@ pvesm list "$storage" >$outfile # faster search, only pull it once
 #echo "$(date) - Processing list of ${#vmlist[@]} VMIDs"
 echo "$(date) - Processing list of VMIDs to get latest backup date"
 for i in ${vmlist[@]}; do
-  echo $i
+  echo -n "$i  "
 #  latestbkp=$(pvesm list "$storage" |grep "/$i/" |tail -n 1)
   latestbkp=$(grep "/$i/" $outfile |tail -n 1)
+
+if [ $restorelxc -gt 0 ] && [ $(echo "$latestbkp" |grep -c pbs-ct) -gt 0 ]; then
   echo "$latestbkp" |tee -a "$restorethese"
-done
+fi
+if [ $restorevm -gt 0 ] && [ $(echo "$latestbkp" |grep -c pbs-vm) -gt 0 ]; then
+  echo "$latestbkp" |tee -a "$restorethese"
+fi
+echo ''
+done |awk 'NF>0'
 
 #using-my-own-vm-as-storage-lol:backup/ct/105/2025-02-04T10:00:05Z  pbs-ct  backup       640117954 105
 #using-my-own-vm-as-storage-lol:backup/ct/105/2025-02-05T10:00:13Z  pbs-ct  backup       640122782 105
@@ -66,9 +78,9 @@ function commanum () {
 } 2>/dev/null
 
 echo '========================='
-echo "Number of VMs to restore: $(wc -l $restorethese)"
-echo "$(basename $0) - $(date) - Space needed to restore (MiB): $(commanum $spaceneed1024)" |tee -a $logf
-echo "Or $spaceneedgig GiB"
+echo "Number of LXC/VMs to restore: $(wc -l $restorethese)"
+echo "$(basename $0) - $(date) - Space needed to restore (MiB): $(commanum $spaceneed1024) / $spaceneedgig GiB " |tee -a $logf
+echo "NOTE this can vary with zfs compression on destination storage"
 echo '========================='
 echo "Enter to begin restore to $dest or ^C"
 echo "PROTIP: You can edit the list in another window before proceeding"
@@ -76,16 +88,36 @@ ls -lh $restorethese
 read
 
 # Teh Main Thing
-for bkp in $(awk '{print $1}' $restorethese); do
+sdate=$(date)
+oifs=$IFS
+IFS="
+"
+#for line in $(cat $restorethese); do
+# NOTE this will still proceed to the next one if restore fails
+while read -r line; do
+  bkp=$(echo "$line" |awk '{print $1}')
+  vmtype=$(echo "$line" |awk '{print $2}')
   let newid=$(pvesh get /cluster/nextid) # get next free VMID number
+
   echo "$(date) - Restoring to $dest -- Image dated: $bkp -- as VMID $newid"  |tee -a $logf
 if [ $TESTONLY -eq 0 ]; then
-  time qmrestore --storage $dest "$bkp" $newid \
-  ||echo "ERROR: $(date) - Restore of $bkp / new VMID $newid failed!" |tee -a $logf
+  if [ $vmtype = "pbs-vm" ]; then
+    time qmrestore --storage $dest "$bkp" $newid \
+    ||echo "ERROR: $(date) - Restore of VM $bkp / new VMID $newid failed!" |tee -a $logf
+  else
+    time pct restore $newid --storage $dest "$bkp" \
+    ||echo "ERROR: $(date) - Restore of LXC $bkp / new VMID $newid failed!" |tee -a $logf
+  fi
 fi
 # PROTIP: add "--unique 1" if you need a new MAC address for each VM (usually only necessary if VM is duplicated)
   date |tee -a $logf
-done
+done < $restorethese
+endate=$(date)
+IFS=$oifs
+
+echo '=========================' |tee -a $logf
+echo "Restore process started: $sdate" |tee -a $logf
+echo "Restore process   ended: $endate" |tee -a $logf
 echo '=========================' |tee -a $logf
 ls -l $logf
 
@@ -101,3 +133,6 @@ ls -l $logf
 #Fri Feb  7 12:17:55 AM MST 2025
 #Fri Feb  7 12:17:58 AM MST 2025 - Restoring to xfs-exos10 -- Image dated: pbsvm3-25g-vm-on-beelink:backup/vm/103/2025-02-02T08:02:36Z -- as VMID 138
 #Fri Feb  7 12:18:02 AM MST 2025
+
+# HOWTO cleanup a test restore of LXC:
+# for VMID in 130 131; do pct set --protection 0 $VMID; echo $VMID; time pct destroy $VMID; done; date
