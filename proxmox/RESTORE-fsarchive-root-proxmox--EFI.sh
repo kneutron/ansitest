178a3613413f7603fcaca87dd1a00c7e819534cf
kneutron <50146127+kneutron@users.noreply.github.com>
2025-05-24 19:14:14 -0600

    Add files via upload

--- proxmox/RESTORE-fsarchive-root-proxmox--EFI.sh
+++ proxmox/RESTORE-fsarchive-root-proxmox--EFI.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-# 2024.0419 kneutron
+# 2025.May / 2024.0419 kneutron
 # Mod for proxmox LVM+ext4 root
 # EFI version
 
-if [ "$1" = "help" ]; then
-(cat <<EOF
-(NOTE you are in the 'less' pager, hit 'q' to quit)
+# To be run from systemrescuecd environment; NOTE restore disk MUST be partitioned 1st!
 
-To be run from systemrescuecd environment; NOTE restore disk MUST be partitioned 1st!
+# Standard ext4+lvm install with no lvm-thin:
+#
+#Disk /dev/vda: 32 GiB, 34359738368 bytes, 67108864 sectors
+#Sector size (logical/physical): 512 bytes / 512 bytes
+#I/O size (minimum/optimal): 512 bytes / 512 bytes
+#Disklabel type: gpt
+#
+#Device       Start      End  Sectors  Size Type
+#/dev/vda1       34     2047     2014 1007K BIOS boot
+#/dev/vda2     2048  1050623  1048576  512M EFI System
+#/dev/vda3  1050624 67108830 66058207 31.5G Linux LVM
 
-This script REQUIRES 1 arg: filename of .fsa
-Copy this script to /tmp and chmod +x, run from there
+# ===========
 
-If you prefer not to use an ISO to restore from, systemrescuecd has sshfs:
+# This script REQUIRES 1 arg: filename of .fsa
+# copy this script to /tmp and chmod +x, run from there
 
-HOWTO mount your backup storage over sshfs:
+# If you prefer not to use an ISO to restore from, systemrescuecd has sshfs:
+
+# HOWTO mount your backup storage over sshfs:
 # sshfs -C -o Ciphers=chacha20-poly1305@openssh.com  loginid@ipaddress:/path/to/backupfile \
 #  /mnt/path \
 #  -o follow_symlinks
@@ -23,51 +33,8 @@ HOWTO mount your backup storage over sshfs:
 # mkdir -pv /mnt/restore; sshfs -C -o Ciphers=chacha20-poly1305@openssh.com dave@192.168.1.185:/mnt/seatera4-xfs/notshrcompr/bkpsys-proxmox \
 #   /mnt/restore -o follow_symlinks
 
-If using sshfs, cd to restore-dir and run /tmp/$0
+# If using sshfs, cd to restore-dir and run /tmp/$0
 
-PROTIP - you can run ' screen ' or ' tmux ' from systemrescuecd and have this help text visible while restoring!
-
-============
-Full restore instructions: (You may want to print this or copy to text editor, or have it up in a web browser)
-
-You need to have downloaded:
-o Proxmox VE ISO installer - https://www.proxmox.com/en/downloads
-o Systemrescuecd  	   - https://distrowatch.com/table.php?distribution=systemrescue
-o Super Grub Disc (emergency fix boot) - https://distrowatch.com/table.php?distribution=supergrub
-
-o Also handy to have - Rescatux: https://distrowatch.com/table.php?distribution=rescatux
-
-Run the Proxmox installer from ISO / USB and recreate the LVM + ext4 root FS with appropriate disk size 
-
-Then, Boot systemrescuecd
-
-cd /tmp
-Fire up ' mc ' Midnight Commander
-
-Tab to right pane, Esc+9 (or F9) and SFTP to where your .fsa backup file(s) are
-
-SCP the appropriate restore script (EFI or NO-EFI) over to local /tmp and ' chmod +x ' it
-
-EDIT THIS SCRIPT (look for EDITME) and change the appropriate values to match your restore environnment
-
-Follow the sshfs HOWTO in the comment at the beginning of this script to mount your backup file storage
-
-cd /mnt/restore
-/tmp/$0 backupfilename.fsa # Start the restore
-
-After the restore finishes: From here you can still chroot into the /mnt/tmp2 dir and disable zfs imports, edit /etc/fstab, et al
-
-Shutdown, remove rescue media and it should reboot into Proxmox VE
-If not, boot Super Grub Disc and that should do it
-
-Once you have a PVE login prompt, you can login as root and reinstall grub
-' grub-install /dev/blah '
-
-and then do a test reboot to make sure everything comes up as expected.
-EOF
-) |less
-exit 0;
-fi
 
 # failexit.mrg
 function failexit () {
@@ -75,7 +42,6 @@ function failexit () {
   exit $1
 }
 
-# needed for proxmox LVM
 vgchange -a y
 
 # TODO editme
@@ -83,7 +49,7 @@ vgchange -a y
 #rootdev=/dev/vda3 # for restore to VM
 rootdev=/dev/mapper/pve-root # for proxmox ext4+LVM restore
 
-# TODO editme if needed - this is for restoring to proxmox VM with virtio disk
+# TODO editme
 efidev=/dev/vda2
 
 # FIX
@@ -99,28 +65,32 @@ mkdir -pv $rootdir
 myres=BOOJUM-RESTORE.sh # injection script
 
 # NOTE sr0 is systemrescue
-#mount /dev/sr1 $cdr -oro
-#chkmount=`df |grep -c $cdr`
-#[ $chkmount -gt 0 ] || failexit 99 "Failed to mount $cdr"; # failed to mount
-chkmount=`df |grep -c $rootdir`
-[ $chkmount -eq 0 ] || failexit 98 "$rootdir is still mounted - cannot restore!";
+chkmount=$(df |grep -c $rootdir)
+[ $chkmount -eq 0 ] || failexit 5 "$rootdir is still mounted - cannot restore!";
 
-#cd $cdr || failexit 199 "Cannot CD to $cdr";
 pwd
 
 echo "$(date) - Restoring EFI partition to $efidev"
+[ -e boot-efi.dd.gz ] && time gzip -cd boot-efi.dd.gz > $efidev
 [ -e boot-efi.dd.lzop ] && time lzop -cd boot-efi.dd.lzop > $efidev
-[ -e boot-efi.dd ] && time dd if=boot-efi.dd of=$efidev bs=1M status=progress > $efidev
+[ -e boot-efi.dd ] && time dd if=boot-efi.dd of=$efidev bs=1M status=progress 
 
 # debugg
 #read -n 1 -p "PK"
 
-echo "`date` - RESTORING root filesystem to $rootdev"
+echo "$(date) - RESTORING root filesystem to $rootdev"
 
 # PROBLEM with long filenames in UDF - gets cut off, use $1
 #time fsarchiver restfs *.fsa id=0,dest=$rootdev
 
-time fsarchiver restfs -v "$1" id=0,dest=$rootdev  || failexit 400 "Restore to $rootdev failed!";
+time fsarchiver restfs -v "$1" id=0,dest=$rootdev  || failexit 40 "Restore to $rootdev failed!";
+
+# TODO fix
+#while `wait -n`; do ## notwork - endless loop
+#while [ `jobs |wc -l` -gt 0 ]; do
+#  df -h |grep $rootdev # grep fsa
+#  sleep 5
+#done
 
 date
 
@@ -158,9 +128,9 @@ mount -o bind /dev $rootdir/dev; mount -o bind /proc $rootdir/proc; mount -o bin
  
 # FIXED
 myres2=$rootdir/$myres
-touch $myres2 || failexit 298 "Check if R/O filesystem?"
+touch $myres2 || failexit 98 "Check if R/O filesystem?"
 
-echo "#!/bin/bash" > $myres2 || failexit 299 "Cannot update $myres2 injection script - Check R/O filesystem?"
+echo "#!/bin/bash" > $myres2 || failexit 99 "Cannot update $myres2 injection script - Check R/O filesystem?"
 echo "mount /boot/efi" >>$myres2
 echo "grub-install $rtdevonly" >> $myres2  # from chroot
 echo "update-grub" >> $myres2
@@ -190,10 +160,43 @@ echo "- Also remove anything from /etc/fstab that exists on the host but not in-
 
 exit;
 
-# 2024.0419 kneutron
+
+# 2025.May / 2024.0419 kneutron
 
 =======================================
+Full restore instructions:
 
-2024.0508 Added help screen  
+Run the Proxmox installer from ISO / USB and recreate the LVM + ext4 root FS with appropriate disk size 
+
+Boot systemrescuecd
+
+cd /tmp
+Fire up ' mc ' Midnight Commander
+
+Tab to right pane, Esc+9 (or F9) and SFTP to where your .fsa backup file(s) are
+
+SCP the appropriate restore script over to local /tmp and ' chmod +x ' it
+
+EDIT THIS SCRIPT (look for EDITME) and change the appropriate values to match your restore environnment
+
+Follow the sshfs HOWTO in the comment at the beginning of this script to mount your backup file storage
+
+cd /mnt/restore
+/tmp/$0 backupfilename.fsa # Start the restore
+
+From here you can still chroot into the /mnt/tmp2 dir and disable zfs imports, edit /etc/fstab, et al
+
+Shutdown, remove rescue media and it should reboot into Proxmox VE
+If not, boot Super Grub Disc and that should do it
+
+Once you have a PVE login prompt, you can login as root and reinstall grub
+' grub-install /dev/blah '
+
+and then do a test reboot to make sure everything comes up as expected.
+
+=======================================
+  
+########
+
 2024.0419 Tested, works OK with proxmox lvm
 2017.0827 Tested, works OK
