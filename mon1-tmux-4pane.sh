fc701b654f18dc232b5b2c1077e5d7628321a90b
kneutron <50146127+kneutron@users.noreply.github.com>
2025-09-01 11:39:33 -0600

    Update mon1-tmux-4pane.sh

--- mon1-tmux-4pane.sh
+++ mon1-tmux-4pane.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# REQUIRES: tmux bwm-ng sysstat
+
 tmux new-session \; \
-  send-keys 'topp' C-m \; \
+  send-keys 'top -d 7' C-m \; \
   split-window -v -p 70 \; \
-  send-keys 'bwmm' C-m \; \
+  send-keys 'bwm-ng -t 2000' C-m \; \
   split-window -v \; \
-  send-keys 'ziostatt' C-m \; \
+  send-keys 'S_COLORS=never iostat -k 5 -s -y -z' C-m \; \
   select-pane -t 1 \; \
   split-window -h \; \
-  send-keys 'watch -n 61 /home/dave/bin/mon1-df-short' C-m \;
+  send-keys 'watch -n 61 /usr/local/bin/mon1-df-short' C-m \;
