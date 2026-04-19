520ad5ef8895221743fa1f84068637346f0a3588
kneutron <50146127+kneutron@users.noreply.github.com>
2026-04-18 22:34:07 -0600

    Add files via upload

---
+++ ansible-sshcopyid-nopass.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# provide pass non-interactively to ssh-copy-id to setup instance for ansible control/access
+
+# xxx TODO EDITME
+export SSHPASS="87654321"
+
+# xxx TODO EDITME
+for SERVER in server1 server2; do
+  sshpass -e ssh-copy-id -o StrictHostKeyChecking=no avac_admin@$SERVER
+done   
