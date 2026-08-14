64bdc74e125cf581e736f14161793d7936ad2980
kneutron <50146127+kneutron@users.noreply.github.com>
2026-08-14 15:28:01 -0600

    Add files via upload

--- ansible/ansible-query-date--known-hosts.sh
+++ ansible/ansible-query-date--known-hosts.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-cmd="date"
+# FIX UTC: # sudo timedatectl set-timezone "America/Denver"
+
+cmd="timedatectl |paste - -; date"
 #ansible rhel8 -a "$cmd"
 #echo '-----'
 for stanza in $(grep "^\[" /etc/ansible/hosts |grep -v vars); do
 #ansible rhel -a "$cmd"
  echo $stanza
  st2=$(echo "$stanza" |tr -d '[]')
- ansible $st2 -a "$cmd"
+ ansible $st2 -m shell -a "$cmd"
  echo "^^ $st2 -----"
 #ansible debian -a "$cmd"
 #echo '-----'
